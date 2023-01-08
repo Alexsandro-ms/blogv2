ff665a7457eaedd0c1e34c9720746b6ca04e0c49
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-08 17:49:00 -0300

    feat(usermodel): created model of user

---
+++ src/app/models/UserModel.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize");
+const connetion = require("../../utils/connetionDatabase");
+
+const UserModel = connetion.define("users", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+UserModel.sync();
+
+module.exports = UserModel;
