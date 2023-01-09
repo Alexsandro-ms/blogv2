e719cc2f000c1c5c4b9c6596d27c954c0d15e4b7
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-09 13:18:26 -0300

    refactor(userlogin): refactored user login route error handling
    added error handling for: user not registered in the database, empty fields and incorrect email or
    password

--- src/app/useCases/Users/UserLogin.js
+++ src/app/useCases/Users/UserLogin.js
@@ -4,29 +4,41 @@ const bcrypt = require("bcryptjs");
 
 const userLogin = (req, res) => {
   const { email, password } = req.body;
-  UserModel.findOne({
-    where: {
-      email
-    }
-  })
-    .then((user) => {
-      if (user != undefined) {
-        const passwordValidation = bcrypt.compareSync(password, user.password);
-        if (passwordValidation) {
-          req.session.user = {
-            id: user.id,
-            name: user.name,
-            email: user.email
-          };
-          res
-            .status(200)
-            .json({ message: "User has been successfully logged in!" });
-        }
+  if (email && password != "") {
+    UserModel.findOne({
+      where: {
+        email
       }
     })
-    .catch((err) => {
-      console.log(err);
-    });
+      .then((user) => {
+        if (user != undefined) {
+          const passwordValidation = bcrypt.compareSync(
+            password,
+            user.password
+          );
+          if (passwordValidation) {
+            req.session.user = {
+              id: user.id,
+              email: user.email
+            };
+            return res
+              .status(200)
+              .json({ message: "User has been successfully logged in!" });
+          } else {
+            return res
+              .status(401)
+              .json({ message: "Incorrect email or password" });
+          }
+        } else {
+          return res.status(404).json({ message: "User does not exist" });
+        }
+      })
+      .catch((err) => {
+        return res.status(401).json({ message: err });
+      });
+  } else {
+    return res.status(400).json({ message: "fill in all fields" });
+  }
 };
 
 module.exports = userLogin;
