f1af10728f58389532d3e386f3a142c20113c236
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-09 20:06:14 -0300

    refactor(postcreate): refactored code for creating the post with id of use

--- src/app/useCases/Posts/PostCreate.js
+++ src/app/useCases/Posts/PostCreate.js
@@ -1,20 +1,31 @@
 const PostModel = require("../../models/PostModel");
 const slugify = require("slugify");
+const UserModel = require("../../models/UserModel");
 
 const postCreate = (req, res) => {
-  const { title, slug, content } = req.body;
-  if (title != "" && slug != "" && content != "") {
-    PostModel.create({
-      title,
-      slug: slugify(title),
-      content
-    })
-      .then(() => {
-        res.status(201).json({ message: "Post created successfully!" });
+  const { title, content, userId } = req.body;
+
+  if (title != "" && userId != undefined && content != "") {
+    if (isNaN(userId)) {
+      return res
+        .status(400)
+        .json({ message: "Id incorrect or not registered" });
+    } else {
+      PostModel.create({
+        title,
+        slug: slugify(title),
+        content,
+        userId
       })
-      .catch((err) => {
-        res.send(400).json({ message: "Error try again!" });
-      });
+        .then(() => {
+          return res
+            .status(201)
+            .json({ message: "Post created successfully!" });
+        })
+        .catch((err) => {
+          return res.status(400).json({ message: "Error try again!" });
+        });
+    }
   } else {
     res.status(400).json({ message: "Fill in all fields to proceed..." });
   }
